cd6d07e730216270ea437c982acac724f5c5ee81
Marcelo Vega <mavegaf@gmail.com>
2025-05-22 23:06:40 -0400

    adding card

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <main className="max-w-2xl mx-auto mt-10 space-y-4 p-4">
-      <header>
-        <h1 className="text-2xl font-bold">Sun time</h1>
-      </header>
+
+      <Card>
+  <CardHeader>
+    <CardTitle>Sun time</CardTitle>
+    <CardDescription>Look for a location</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+
     </main>
   )
 }
